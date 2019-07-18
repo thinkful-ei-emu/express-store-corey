0b140a51d4cac1181d6b7ffc13c269e7643f5757
Corey Moore <monorthwest@gmail.com>
2019-07-18 13:12:57 -0700

    added GET, POST, DELETE endpoints

--- src/app.js
+++ src/app.js
@@ -4,16 +4,131 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const uuid = require('uuid/v4');
 
 const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+const users = [
+  {
+    'id': '3c8da4d5-1597-46e7-baa1-e402aed70d80',
+    'username': 'sallyStudent',
+    'password': 'c00d1ng1sc00l',
+    'favoriteClub': 'Cache Valley Stone Society',
+    'newsLetter': 'true'
+  },
+  {
+    'id': 'ce20079c-2326-4f17-8ac4-f617bfd28b7f',
+    'username': 'johnBlocton',
+    'password': 'veryg00dpassw0rd',
+    'favoriteClub': 'Salt City Curling Club',
+    'newsLetter': 'false'
+  }
+];
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.get('/', (req, res) => { 
-  res.send('Hello, world!'); 
+
+
+app.post('/register', (req, res) => {
+
+  //get the data
+  const { username, password, favoriteClub, newsLetter=false } = req.body;
+
+  //validation code here
+  if (!username) {
+    return res
+      .status(400)
+      .send('Username required');
+  }
+  if (!password) {
+    return res
+      .status(400)
+      .send('Password required');
+  }
+  if (!favoriteClub) {
+    return res
+      .status(400)
+      .send('Favorite club required');
+  }
+  if (username.length < 6 || username.length > 20) {
+    return res
+      .status(400)
+      .send('User name must be between 6 and 20 characters');
+  }
+  if (password.length < 8 || password.length > 36) {
+    return res
+      .status(400)
+      .send('Password must be between 8 and 36 characters');
+  }
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/))  {
+    return res
+      .status(400)
+      .send('Password must contain at least one didgit');
+  }
+
+  const clubs = [
+    'Cache Valley Stone Society',
+    'Ogden Curling Club',
+    'Park City Curling Club',
+    'Salt City Curling Club',
+    'Utah Olympic Oval Curling Club'
+  ];
+
+  if (!clubs.includes(favoriteClub)) {
+    return res
+      .status(400)
+      .send('Not a valid club');
+  }
+  const id = uuid();
+  const newUser = {
+    id,
+    username,
+    password,
+    favoriteClub,
+    newsLetter
+  };
+
+  users.push(newUser);
+
+  res
+    .status(201)
+    .location(`http://localhost:8000/user/${id}`)
+  //.json(newUser)
+    .json({id:id});
 });
+
+
+
+app.delete('/user/:userId', (req, res) => {
+  const { userId } = req.params;
+  const index = users.findIndex(u => u.id === userId);
+
+  //make sure we actually find a user with that id
+  if (index === -1) {
+    return res
+      .status(404)
+      .send('User not found');
+  }
+  users.splice(index,1);
+
+  res
+    .status(204)
+    .end();
+});
+
+
+
+app.get('/user', (req, res) => {
+  res
+    .json(users);
+});
+
+
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
